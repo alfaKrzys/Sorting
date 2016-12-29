84f15ae55fcfcde7211db9a717e5f2b6875edb9b
alfaKrzys <os_alfa@wp.pl>
2016-12-29 23:26:48 +0100

    Test of generic types

---
+++ Sorting/BubbleSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    /// <summary>
+    /// Class used to sort simple type.
+    /// </summary>
+    /// <typeparam name="T">Sorting Type</typeparam>
+    class BubbleSort<T>
+        where T : IComparable
+    {
+        /// <summary>
+        /// Method is using bubble sort.
+        /// </summary>
+        /// <param name="items">Items to sorted.</param>
+        /// <returns></returns>
+        internal T[] Sort(T[] items)
+        {
+            T temp;// = new T();
+            bool isSorted = false;
+            do
+            {
+                isSorted = true;
+                for (int i = 0; i < items.Length - 1; i++)
+                {
+                    if (items[i].CompareTo(items[i + 1]) > 0)
+                    {
+                        temp = items[i];
+                        items[i] = items[i + 1];
+                        items[i + 1] = temp;
+                        isSorted = false;
+                    }
+                }
+            } while (!isSorted);
+            return items;
+        }
+    }
+}
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,15 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
+            //double[] tab = { 5.4, 6.1, 2.0, 1.2, 5.5 };
+            int[] tab = { 1, 3, 4, 6, 12, 312, 5, 1213, 2, 54354, 2, 66, 444, 23, 6567, 2324, 12123, 36899, 7665, 7566, 75765, 444 };
+            BubbleSort<int> bubbleSort = new BubbleSort<int>();
+            bubbleSort.Sort(tab);
+            foreach (int i in tab)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
         }
     }
 }
